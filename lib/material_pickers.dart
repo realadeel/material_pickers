f523c5b50a0030d5189534634fb882b1efc9f51a
realadeel <adeel.rb@gmail.com>
2020-04-03 01:57:56 -0500

    comment out unnecessary code that had type issues

--- lib/material_pickers.dart
+++ lib/material_pickers.dart
@@ -1,8 +1,7 @@
 library material_pickers;
 
 export 'package:material_pickers/src/color/color_picker.dart' show ColorPicker;
-export 'package:material_pickers/src/color/dropdown.dart'
-    show ColorPickerButton;
+export 'package:material_pickers/src/color/dropdown.dart' show ColorPickerButton;
 export 'package:material_pickers/src/font/font_picker.dart';
 export 'package:material_pickers/src/picker.dart';
 export 'package:material_pickers/src/picker.dart' show DisplayType;
